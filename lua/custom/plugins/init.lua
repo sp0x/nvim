66ba1cd9ce120c4b20b9adf930c4418f3968a3f8
vasil <vasil.vasilevext@delaval.com>
2025-01-28 13:59:31 +0100

    add: better lsp integration

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -164,4 +164,12 @@ return {
       'nvim-tree/nvim-web-devicons', -- optional
     },
   },
+  {
+    'ray-x/lsp_signature.nvim',
+    event = 'VeryLazy',
+    opts = {},
+    config = function(_, opts)
+      require('lsp_signature').setup(opts)
+    end,
+  },
 }
