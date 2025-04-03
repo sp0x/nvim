167d0d13ecb5aca1be7e004fcb458251fe37d1bd
vasil <vasil.vasilevext@delaval.com>
2025-04-03 09:04:40 +0200

    change cursor

--- lua/custom/plugins/smear_cursor.lua
+++ lua/custom/plugins/smear_cursor.lua
@@ -2,6 +2,14 @@ return {
   'sphamba/smear-cursor.nvim',
   opts = {},
   config = function()
-    require('smear_cursor').setup()
+    require('smear_cursor').setup {
+      stiffness = 0.5,
+      trailing_stiffness = 0.5,
+      distance_stop_animating = 0.5,
+      cursor_color = '#ff8800',
+      trailing_exponent = 5,
+      hide_target_hack = true,
+      gamma = 1,
+    }
   end,
 }
